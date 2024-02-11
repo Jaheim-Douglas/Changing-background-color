9cd4180c987ea25833df5f995607058fb8dc3184
Jaheim Douglas <141573299+Jaheim-Douglas@users.noreply.github.com>
2024-02-11 10:03:41 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function greenBackground() {
+    if (document.body.style.backgroundColor == "white" || document.body.style.backgroundColor == "red") {
+        document.body.style.backgroundColor = "green";
+    } else {
+        document.body.style.backgroundColor = "white";
+    }
+}
+
+function redBackground() {
+    if (document.body.style.backgroundColor == "white" || document.body.style.backgroundColor == "green") {
+        document.body.style.backgroundColor = "red";
+    } else {
+        document.body.style.backgroundColor = "white";
+    }
+}
+
+
+
